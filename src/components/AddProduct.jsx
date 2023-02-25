1ce6174997d6f470ecaaad7f3e530cb87b08cac6
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 19:35:21 +0300

    button2 import

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import {FaCartPlus} from "react-icons/fa"
+import Button2 from "./Button2"
+
+const AddProduct = () => {
+  return (
+    <div>
+      <Button2/>
+    </div>
+  )
+}
+
+export default AddProduct
