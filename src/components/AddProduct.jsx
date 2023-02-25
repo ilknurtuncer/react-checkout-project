a129104373e515741ccef12ed590403cb3f68baa
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 19:37:20 +0300

    onclick added

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -3,9 +3,10 @@ import {FaCartPlus} from "react-icons/fa"
 import Button2 from "./Button2"
 
 const AddProduct = () => {
+  const [button2, setButton2] = useState(true)
   return (
-    <div>
-      <Button2/>
+    <div onClick={(e)=> setButton2(!button2)}>
+      {button2 ? <Button2/> : null}
     </div>
   )
 }
