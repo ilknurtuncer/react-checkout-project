64c3f10867f12da3177c105b27d49fd58e2e0c8c
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 19:46:21 +0300

    button2.jsx created

---
+++ src/components/Button2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const 
+ = () => {
+  return (
+    <div>
+    <div className="container text-center">
+        <button className="bg-warning p-2" style={{borderRadius:"5px" , border:"none"}}>Hide Product Bar</button>
+    </div>
+    </div>
+  )
+}
+
+export default 
