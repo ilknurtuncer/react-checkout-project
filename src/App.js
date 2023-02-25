55ffb15ea45be53d5c90f5042878d86bef73793b
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 16:59:40 +0300

    header data main import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// import Main from "./pages/Main";
-// import data from "./helper/data"
-// import Header from "./components/Header";
+import Main from "./pages/Main";
+import data from "./helper/data"
+import Header from "./components/Header";
 
-// function App() {
-//   console.log(data);
-//   return (
-//     <>
-//       <Header/>
-//       <Main data={data}/>
-//     </>
+function App() {
+  console.log(data);
+  return (
+    <>
+      <Header/>
+      <Main data={data}/>
+    </>
   );
 }
 
