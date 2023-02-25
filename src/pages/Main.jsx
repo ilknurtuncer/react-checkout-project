958759bfa88282ad87b535baa80a28977891f00f
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 19:52:21 +0300

    state  productcard vs.. import

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,12 @@
-
-
+import { useState } from "react"
+import AddProduct from "../components/AddProduct"
+import Button from "../components/Button"
+import CardTotal from "../components/CardTotal"
 import ProductCard from "../components/ProductCard"
 
 
 const Main = ({data}) => {
+  
   return (
     <div>
         {data.map((item)=>{
