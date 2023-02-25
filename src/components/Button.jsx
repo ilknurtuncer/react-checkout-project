8d9043fd517157bb1928f36987905d8df2e331a9
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-25 19:41:47 +0300

    button.jsx  div add

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+      <div className="container text-center">
+          <button className="bg-warning p-2" style={{borderRadius:"5px" , border:"none"}}>Show Product Bar</button>
+      </div>
+    )
+  }
+  
+  export default Button
